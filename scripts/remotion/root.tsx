4b8c59a5be812e137210959ad146bdd004b4651f
Joshbond123 <adeniranj787@gmail.com>
2026-05-03 08:12:26 +0100

    feat: Remotion root entry with calculateMetadata

---
+++ scripts/remotion/root.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Composition, registerRoot } from 'remotion';
+import { TikTokVideo, TikTokVideoProps } from './TikTokVideo';
+
+const defaultProps: TikTokVideoProps = {
+  scenes: [],
+  audioSrc: '',
+  script: '',
+  title: '',
+  durationInFrames: 1500,
+};
+
+const RemotionRoot: React.FC = () => {
+  return (
+    <Composition
+      id="TikTokVideo"
+      component={TikTokVideo}
+      fps={30}
+      width={1080}
+      height={1920}
+      durationInFrames={1500}
+      defaultProps={defaultProps}
+      calculateMetadata={({ props }) => ({
+        durationInFrames: (props as TikTokVideoProps).durationInFrames ?? 1500,
+      })}
+    />
+  );
+};
+
+registerRoot(RemotionRoot);
